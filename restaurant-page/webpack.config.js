ab7fc8febdd576f8ad020c943c65fdd78986a67c
madesh02104 <madesh02104@gmail.com>
2024-07-30 21:34:15 +0530

    fixed some problems in webpack configuration

--- restaurant-page/webpack.config.js
+++ restaurant-page/webpack.config.js
@@ -2,10 +2,15 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
